99eb2299a886145aad5c3ad05cff5ea2ac9338c6
Gazizyan <148913111+Gazizyan@users.noreply.github.com>
2023-11-04 15:02:33 +0300

    Test_1.py

--- Лабораторная работа 2/Test_1.py
+++ Лабораторная работа 2/Test_1.py
@@ -3,12 +3,13 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 number_month = 0
 
 while money_capital > 0:
     money_capital += (salary - spend)
-    spend *= increase
-
+    spend *= (1 + increase)
+    print(money_capital)
     if money_capital > 0:
         number_month += 1
     else:
